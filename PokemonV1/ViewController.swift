0654bf27f5e1266afc2501ee0ae4bc5c844312bd
Eugeniya <bruyko.e@gmai.com>
2022-10-12 14:03:15 -0600

    Initial Commit

---
+++ PokemonV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonV1
+//
+//  Created by brubru on 12.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
